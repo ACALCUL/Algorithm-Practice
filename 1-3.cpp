08681849b626ccff516ba492fb89645f5e365fb1
Lee_Donghyun <105157783+ACALCUL@users.noreply.github.com>
2023-04-20 20:57:56 +0900

    Add files via upload

--- 1-3.cpp
+++ 1-3.cpp
@@ -1,6 +1,27 @@
-#include <iostream>
+#include <bits/stdc++.h>
 using namespace std;
 
-int main(){
-    printf("Helloa\n");
+void exchange(int n, vector<int>& S){
+    int i, j, temp;
+    for(i=1;i<=n;i++)
+        for(j=i+1; j<=n; j++)
+            if(S[j]<S[i]){
+                temp=S[i];
+                S[i]=S[j];
+                S[j]=temp;
+            }
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+    vector<int> S(n+1);
+    for (int i = 1; i <= n; i++)
+        scanf("%d", &S[i]);
+    exchange(n, S);
+    for (int i = 1; i <= n; i++)
+        if (i < n)
+            printf("%d ", S[i]);
+        else
+            printf("%d\n", S[i]);
 }
