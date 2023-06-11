c92e212409cffbb2f8b7b1328b8291ce52f4c0b2
Lee_Donghyun <105157783+ACALCUL@users.noreply.github.com>
2023-06-11 21:36:14 +0900

    Add files via upload

--- 11-3.cpp
+++ 11-3.cpp
@@ -16,32 +16,50 @@ void make_graph(int n, int m){
             for(int k=0; k<8; k++){
                 ni=i+imove[k];
                 nj=j+jmove[k];
+                //(i, j)에 있는 Knight가 이동 가능한 좌표: (ni, nj)
                 if((ni>=0)&&(ni<n)&&(nj>=0)&&(nj<m))
                     graph[i*m+j].push_back(ni*m+nj);
+                //graph[i*m+j]에는 (i, j)좌표에서 Knight가 이동 가능한 좌표를 ni*m+nj형식으로 push
             }
 }
 
 void tour_circuit(int k, int v, int s, int n, int m, vector<int> mark){
     //k: 방문횟수, v: 현재 point, s: 출발 point,
-    //n: 행의 개수, m: 열의 개수, mark: 방문 순서를 저장하는 배열
+    //k, v, s는 원래 2차원이여야 하나 i*m+j형태로 1차원으로 압축함
+    //n: 행의 개수, m: 열의 개수, mark: vertex를 1차원으로 압축한 것에 대한 방문 순서를 저장
 
     if(k==n*m){
         bool opt=false;
         for(int i: graph[v])
             if(i==s){
+            //graph[v] 배열에 시작지점 s와 같은 vertex가 있으면 true
                 opt=true;
                 break;
             }
         if(opt){
             mark[v]=k;
+            
+            /* for(int i=1; i<=k; i++){
+                for(int j=0; j<mark.size(); j++){
+                    if(mark[j]==i){
+                        cout<<"("<<j/m<<", "<<j%m<<") ";
+                        //cout<<j<<" ";
+                        //위는 2차원 형태 아래는 1차원 압축 형태
+                        break;
+                    }
+                }
+            }
+            cout<<endl; */
+            
             cnt++;
+            //cnt는 해밀턴 회로의 개수
         }
     }
     else
-        for(int u: graph[v])
-            if(mark[u]==0){
-                mark[u]=k+1;
-                tour_circuit(k+1, u, s, n, m, mark);
+        for(int u: graph[v])//v에서 Knight가 이동 가능한 모든 점 u에 대해서
+            if(mark[u]==0){//u를 방문한 적 없다면
+                mark[u]=k+1;//u를 다음으로 방문했다고 하고
+                tour_circuit(k+1, u, s, n, m, mark);//그렇게 했을 때를 재귀 호출
                 mark[u]=0;
             }
 }
@@ -51,6 +69,7 @@ void tour_path(int k, int v, int s, int n, int m, vector<int> mark){
     //n: 행의 개수, m: 열의 개수, mark: 방문 순서를 저장하는 배열
 
     if(k==n*m){
+        //해밀텅 회로에서 시작 지점과 같은 vertex가 있어야 한다는 조건만 빠짐
         mark[v]=k;
         cnt++;
     }
